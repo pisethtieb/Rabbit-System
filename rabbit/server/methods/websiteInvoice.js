04cc5ab82ee7106bf229867ddb2c32e749f497ad
pisethtieb <pisethsomaly@hotmail.com>
2016-02-08 03:23:29 -0600

    not fixed invoice

--- rabbit/server/methods/websiteInvoice.js
+++ rabbit/server/methods/websiteInvoice.js
@@ -17,12 +17,33 @@ Meteor.methods({
         /****** Header *****/
         //data.header = params;
         let paymentWebsite = Rabbit.Collection.PaymentWebsite.findOne(paymentWebsiteId);
+        let website = Rabbit.Collection.PaymentWebsite.find({
+            websiteId: paymentWebsite.websiteId
+        });
+        let getPrice = Rabbit.Collection.Website.findOne(paymentWebsite.websiteId);
+        data.buildPrice = getPrice.price;
+        if (paymentWebsite.buildDue == '' || paymentWebsite.buildDue == null) {
+            paymentWebsite.buildDue = 0;
+            data.builSumPaid = getPrice.price - paymentWebsite.buildDue;
+            data.buildDue = paymentWebsite.buildDue;
+            data.footer.totalPaid = data.builSumPaid + paymentWebsite.domainNamePaid + paymentWebsite.hostingPaid + paymentWebsite.maintenancePaid;
+
+        } else {
+            data.builSumPaid = getPrice.price - paymentWebsite.buildDue;
+            data.buildDue = paymentWebsite.buildDue;
+
+
+        }
         data.header = paymentWebsite._customer;
-
-
         data.content = paymentWebsite;
         data.header.paymentDate = paymentWebsite.paymentWebsiteDate;
         data.header.websiteName = paymentWebsite._website.webName;
+
+        let total = getPrice.price + paymentWebsite.domainNamePrice + paymentWebsite.hostingPrice + paymentWebsite.maintenancePrice;
+        let due = paymentWebsite.buildDue + paymentWebsite.domainNameDue + paymentWebsite.hostingDue + paymentWebsite.maintenanceDue;
+
+        data.footer.total = total;
+        data.footer.due = due;
         return data
 
     }
