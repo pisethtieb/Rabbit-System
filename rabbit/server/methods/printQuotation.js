6b76dfe18af56445c87d80773e00b1790e9fa30c
pisethtieb <pisethsomaly@hotmail.com>
2016-03-05 21:11:17 +0700

    quotation is not finish yey

--- rabbit/server/methods/printQuotation.js
+++ rabbit/server/methods/printQuotation.js
@@ -17,12 +17,21 @@ Meteor.methods({
         data.product = quotation._product;
         data.customer = quotation._customer;
         data.contractor = quotation._contractor;
-        data.product.priceHead = quotation.basePrice[0].headOffice;
-        data.product.priceBrand = quotation.basePrice[0].branch;
-        data.product.maintenancePriceHead = quotation.maintenancePrice[0].headOffice;
-        data.product.maintenancePriceBrand = quotation.maintenancePrice[0].branch;
+        if (quotation.type == "fullyFee") {
+            data.types = 'hello';
 
+            data.product.priceHead = quotation.basePrice[0].headOffice;
+            data.product.priceBrand = quotation.basePrice[0].branch;
+            data.product.maintenancePriceHead = quotation.maintenancePrice[0].headOffice;
+            data.product.maintenancePriceBrand = quotation.maintenancePrice[0].branch;
+        } else {
+            data.type = 'hello'
 
+            //data.product.priceHead = quotation.monthlyFee[0].headOffice;
+            //data.product.priceBrand = quotation.basePrice[0].branch;
+            //data.product.maintenancePriceHead = quotation.maintenancePrice[0].headOffice;
+            //data.product.maintenancePriceBrand = quotation.maintenancePrice[0].branch;
+        }
 
         ///****** Header *****/
         ////data.header = params;
